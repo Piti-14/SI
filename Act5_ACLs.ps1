11122b84c02c9b748806134602e0349af73d1e32
Piti! <91146038+Piti-14@users.noreply.github.com>
2023-01-31 19:41:47 +0100

    Add files via upload

---
+++ Act5_ACLs.ps1
@@ -0,0 +1,15 @@
+﻿New-LocalGroup grupo01
+New-LocalGroup grupo02
+New-LocalGroup grupo03
+
+New-LocalUser -FullName J.Cuantico -Password passuser01 -Description Gestor de Proyecto
+New-LocalUser -FullName R.Nvidia -Password passuser02 -Description Analista de BD
+New-LocalUser -FullName L.Puerta Logica -Password passuser03 -Description Programador Web
+
+Add-LocalGroupMember -Group grupo01 -Member J.Cuantico
+Add-LocalGroupMember -Group grupo02 -Member R.Nvidia
+Add-LocalGroupMember -Group grupo03 -Member L.Puerta Logica
+
+------------------------------------------------------------
+
+mkdir -p C:\user02\home\{dir01\{dir03}, dir02}
